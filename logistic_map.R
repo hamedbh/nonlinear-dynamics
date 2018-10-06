38f04efe7413a00e40d7c469e96de309f4a6e91c
Hamed Bastan-Hagh <hamedbh@radlabs-MacBook-Pro.local>
2018-10-06 14:28:26 +0100

    create logistic map results in C++

--- logistic_map.R
+++ logistic_map.R
@@ -1,13 +1,17 @@
+library(Rcpp)
+cppFunction('NumericVector log_map_cpp (NumericVector x, double r) {
+            int n = x.size(), i = 0; n--;
+            for (; i < n; i++) x[i + 1] = r * x[i] * (1 - x[i]);
+            return x;
+            }')
+
 logistic_map <- function(x, # starting condition, 
                          r, # rate parameter, 
                          N  # number of iterations
 ) {
     results <- numeric(length = N + 1)
     results[1] <- x
-    for (i in seq_len(N)) {
-        results[i + 1] <- r * results[i] * (1 - results[i])
-    }
-    
+    log_map_cpp(results, r)
     data.frame(i = c(0, seq_len(N)), 
                x = results)
 }
@@ -16,24 +20,16 @@ plot_logistic_map <- function(x, # starting condition,
                               r, # rate parameter, 
                               N  # number of iterations
 ){
-    plot_title <- paste0("Logistic Map: x0 = ", 
-                         x, 
-                         "; r = ", 
-                         r)
-    
-    results <- numeric(length = N + 1)
-    results[1] <- x
-    for (i in seq_len(N)) {
-        results[i + 1] <- r * results[i] * (1 - results[i])
-    }
-    
-    d <- data.frame(i = c(0, seq_len(N)), 
-                    x = results)
+    plot_subtitle <- paste0("x0 = ", 
+                            x, 
+                            "; r = ", 
+                            r)
+    d <- logistic_map(x = x, r = r, N = N)
     
     ggplot2::ggplot(d, ggplot2::aes(i, x)) + 
         ggplot2::geom_point() + 
         ggplot2::theme_minimal() + 
         ggplot2::scale_y_continuous(limits = c(0, 1), 
                                     breaks = seq(0, 1, by = 0.2)) + 
-        ggplot2::ggtitle(plot_title)
+        ggplot2::ggtitle(label = "Logistic Map", subtitle = plot_subtitle)
 }
